ae6f733341efdec802421b62ff490bf398bbb556
Fabricio Hang <saveasfabri@gmail.com>
2022-11-06 21:27:20 -0300

    :sparkles: Add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,121 @@
-import { Box } from "@chakra-ui/react";
+import shopApp from '../images/shop-app.png';
+import { Link as RouterLink } from 'react-router-dom';
+import { Image, Flex, Input, Text, Link, Icon, Button, InputGroup, InputRightElement } from '@chakra-ui/react';
+import { FaTwitterSquare, FaFacebookSquare, FaInstagramSquare, FaRebel, FaTelegramPlane } from 'react-icons/fa'
+
 
 const Footer = () => {
   return (
-    <Box className="footer">
-      <h3>FOOTER</h3>
-    </Box>
+    <Flex
+      w='100%' maxW='1200px' h='100%'
+      color='gray.600'
+      flexDir={{base:'column', md:'row'}} alignContent='center' justifyContent='center'
+      px={5}
+    >
+      {/* ********************** */}
+      <Flex className='box1'
+        w={{base:'100%', md:'35%'}} h='100%'
+        py={8}
+        flexDir='column'  justifyContent='space-between'>
+        <Image
+          w={{ base: '80px', sm:'100px', md: '100px' }}
+          src={shopApp} alt='Logo'
+        />
+        <Flex>
+          <Icon as={ FaFacebookSquare } 
+          w='25px' h='25px' mr={{base:'5px', md:'15px'}} color='gray.500'
+          _hover={{
+            color:'blue.shop'
+          }}
+          />
+          <Icon as={ FaInstagramSquare } 
+          w='25px' h='25px' mr={{base:'5px', md:'15px'}} color='gray.500'
+          _hover={{
+            color:'blue.shop'
+          }}
+          />
+          <Icon as={ FaTwitterSquare } 
+          w='25px' h='25px' mr={{base:'5px', md:'15px'}} color='gray.500'
+          _hover={{
+            color:'blue.shop'
+          }}
+          />
+        </Flex>
+        <Text fontSize='.7em' color='gray.500'>
+          © All rights reserved
+        </Text>
+      </Flex>
+      {/* ********************** */}
+      <Flex className='box2'
+        w='35%'h='100%'
+        fontSize='.8em' color='gray.500'
+        py={8}
+        flexDir='column'  justifyContent='space-between'
+        >
+          <Text fontSize='1.2em' color='gray.600'>
+            CONTACT US
+          </Text>
+          <Link
+            style={{ textDecoration: 'none' }}       
+            as={RouterLink} to=''
+            _hover={{
+              color:'blue.shop',
+            }}>
+            <Text>lucasranch@skywalker.com</Text>          
+          </Link>
+          <Flex alignItems='center'>
+            <Icon as={ FaRebel } 
+            w='13px' h='13px' mr='5px' color='gray.600'
+            _hover={{
+              color:'blue.shop'
+            }}
+            />
+            <Text>Skywalker Ranch</Text>
+          </Flex>
+          <Text>Lucas Valley-Marinwood, CA - 94946</Text>          
+      </Flex>
+      {/* ********************** */}
+      <Flex className='box3'
+        w='30%'h='100%'
+        fontSize='.8em'
+        color='gray.500'
+        py={8}
+        flexDir='column'  justifyContent='space-between'
+        >
+          <Text fontSize='1.2em' color='gray.600'>
+            SUBSCRIBE NOW
+          </Text>
+          <Text>
+            Sign up if you want to get notifications
+          </Text>
+          <InputGroup size='sm'>
+            <Input
+              borderColor='gray.300'
+              placeholder='Enter you e-mail'
+              _placeholder={{color:'gray.400'}} 
+              focusBorderColor='blue.shop'
+            />
+            <InputRightElement w='3em'>
+              <Button w='2.8rem'h='1.75rem' 
+              mr='1.5px' size='sm' bg='blue.shop'                
+              >
+              <Icon as={ FaTelegramPlane } 
+                w='13px' h='13px'
+                color='white'                
+              />                  
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+          {/* <Input 
+            fontSize={{base:'0.8em', sm:'1.1em'}}
+            _placeholder={{color:'gray.400'}} 
+            bg='gray.200' 
+            variant='filled' placeholder='Search products'
+            size={{base:'sm',md:'md'}} mt='8px'
+            boxShadow='md'
+          /> */}
+      </Flex>
+    </Flex>
   );
 };
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,195 @@
-/* import Logo from './Logo' */
-import { Box } from "@chakra-ui/react";
+import Logo from './Logo';
+import SearchBar from './SearchBar'
+import { Link as RouterLink } from 'react-router-dom';
+import { Flex, useDisclosure, Link, Text, Icon } from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { BiHomeCircle, BiMap, BiChevronDown, BiCart } from 'react-icons/bi'
 
 const NavBar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <Box className="NavBar" w='100%' h='100%' bg='blue.300'> 
-      <h1>Navbar</h1>
-    </Box>
+    <Flex className='NavBar'
+      color='gray.600'
+      w='100%' h='100%'
+      px={5}
+      direction={'column'} alignItems='center'
+      boxShadow='base'
+    >
+      {/* *********************** */}
+      <Flex className='row1'     
+        flexDirection='row' 
+        w='100%' maxW='1200px' h='70%'
+      >
+        <Flex className='logo-container'
+          w={{base:'30%', md:'25%'}}
+          justifyContent='left' 
+          alignItems='center' 
+          >
+          <Logo />
+        </Flex>
+
+        <Flex className='search-container' 
+          w='50%'
+          justify='center' 
+          alignItems='center'
+          >
+          <SearchBar />
+        </Flex>
+
+        <Flex className='cart-icon' 
+          w={{base:'20%', md:'25%'}} h='100%'
+          mt='8px'
+          justifyContent='right' alignItems='center'
+          >
+          <Icon as={ BiCart } 
+            w='25px' h='25px'
+          />
+        </Flex>
+      </Flex>
+      {/* ********************** */}
+      <Flex className='row2'
+        w='100%' maxW='1200px' h='30%'
+        flexDirection='row'
+        >
+        <Flex 
+          w={{base:'100%', md:'30%',lg:'25%'}} h='100%'
+          justifyContent='left' alignItems='center'
+        >
+          <Flex w={{base:'70%', md:'100%'}}>
+            <Icon as={ BiMap } w='18px' h='18px' color='gray.500' />          
+            <Text fontSize={{base:'.8em', lg:'1em'}} color='gray.500'>
+              Capital Federal
+            </Text>
+            <Icon as={ BiChevronDown } 
+            w='20px' h='20px' marginLeft={{base:'5px', md:'15px'}} color='gray.500'
+            />
+          </Flex>
+          <Flex w={{base:'30%'}} justify='right'> 
+            {isOpen ? (
+              <CloseIcon 
+                onClick={isOpen ? onClose : onOpen}
+                display={{ base: 'inline', md: 'none' }}              
+              />
+            ) : (
+            <HamburgerIcon w='20px' h='20px'
+              onClick={isOpen ? onClose : onOpen}
+              display={{ base: 'inline', md: 'none' }}
+            />          
+            )}
+          </Flex> 
+        </Flex>
+
+        <Flex className='menu-container'          
+          w={{md:'70%',lg:'75%'}} h={{base:'300px',md:'100%'}}
+          display={{ base: isOpen ? "flex" : "none", sm: "flex" }}
+          dir={{base:'column', md:'row'}} alignItems='center' justify={{base:'center', md:'left'}}
+          >
+                  
+          <Flex className='home-menu-login'
+            w='100%'
+            alignItems='center' justify='left'
+            >
+            <Link className='home'
+              w='30%'
+              color='gray.600'
+              display='flex' flexDir='row' alignItems='center' justify='left'
+              style={{ textDecoration: 'none' }}       
+              as={RouterLink} to='/'
+              _hover={{
+                color:'blue.shop',
+              }}
+              > 
+              <Icon as={ BiHomeCircle } 
+                w='18px' h='18px' marginRight='5px'
+              />
+              <Text>            
+                Home
+              </Text>
+            </Link>
+            <Flex className='menu'
+            w='70%'            
+              alignItems='center' justifyContent='left'>
+              <Link className='women'
+                alignItems='center'
+                style={{ textDecoration: 'none' }}
+                color='gray.600'
+                as={RouterLink} to='/'
+                _hover={{
+                  color:'blue.shop',
+                }}
+                > 
+                <Text marginRight='15px'>
+                  Women
+                </Text>
+              </Link>
+              <Link className='men'
+                alignItems='center'
+                style={{ textDecoration: 'none' }}
+                variant='outline'
+                color='gray.600'
+                as={RouterLink}
+                to='/'
+                _hover={{
+                  color:'blue.shop',
+                }}
+                > 
+                <Text marginRight='15px'>
+                  Men
+                </Text>
+              </Link>
+              <Link className='kids'
+                alignItems='center'
+                style={{ textDecoration: 'none' }}
+                variant='outline'
+                color='gray.600'
+                as={RouterLink}
+                to='/'
+                _hover={{
+                  color:'blue.shop',
+                }}
+                > 
+                <Text marginRight='15px'>
+                  Kids
+                </Text>
+              </Link>
+            </Flex>         
+          </Flex>
+          <Flex className='login-container'
+            display={{ base: isOpen ? "flex" : "none", md: "flex" }}
+            w='30%' h='100%' 
+            justifyContent='right' alignItems='center' 
+            >
+              <Link className='login'
+                alignItems='center'
+                style={{ textDecoration: 'none' }}
+                as={RouterLink} to='/'
+                _hover={{
+                  color:'blue.shop',
+                }}
+                > 
+                <Text marginLeft='20px'>
+                  Login
+                </Text>
+              </Link>
+              <Link className='sign-up'
+                alignItems='center'
+                style={{ textDecoration: 'none' }}
+                as={RouterLink} to='/'
+                _hover={{
+                  color:'blue.shop',
+                }}
+                > 
+                <Text marginLeft='20px'>
+                  Sign Up
+                </Text>
+              </Link>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 };
+
 
 export default NavBar;
